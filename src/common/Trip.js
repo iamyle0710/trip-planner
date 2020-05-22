d52ec67aff2303c52d712e02fafccba6ef98f3ad
Eric <linmingy@usc.edu>
2020-05-22 13:56:05 -0700

    add Trip model

---
+++ src/common/Trip.js
@@ -0,0 +1,39 @@
+import Todo from './Todo';
+
+export default class Trip {
+	constructor({
+		id,
+		title,
+		category,
+		destination,
+		description,
+		startDate,
+		endDate,
+		todos,
+		reminder,
+		status,
+	}) {
+		this.id = id;
+		this.title = title;
+		this.category = category;
+		this.destination = destination;
+		this.description = description;
+		this.startDate = new Date(startDate);
+		this.endDate = new Date(endDate);
+		this.todos = Array.isArray(todos) ? todos.map((todo) => new Todo(todo)) : [];
+		this.reminder = new Date(reminder);
+		this.status = status;
+	}
+
+	addTodo = (data) => {
+		const todo = new Todo(data);
+		this.todos.push(todo);
+	};
+
+	removeTodo = ({ id }) => {
+		const index = this.todos.findIndex((todo) => todo.id === id);
+		if (index !== -1) {
+			this.todos.splice(index, 1);
+		}
+	};
+}
