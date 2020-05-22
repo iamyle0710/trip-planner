cb4a589a7115b27af156d963040433985b7bbf27
Eric <linmingy@usc.edu>
2020-05-22 15:13:36 -0700

    add helper function to filter trips

---
+++ src/utils/helper.js
@@ -0,0 +1,16 @@
+import Constant from '../common/Constant';
+
+const { CATEGORY } = Constant;
+
+export const filterTrips = (trips = [], searchKeyword = '', filterCategory = CATEGORY.NONE) =>
+	trips.filter((trip) => {
+		const keyword = searchKeyword.toLowerCase();
+		const isCategoryMatched =
+			filterCategory === CATEGORY.NONE || trip.category === filterCategory;
+		const isSearchMatched =
+			[...trip.todos.map((todo) => todo.name), trip.title, trip.destination].findIndex(
+				(str) => str.toLowerCase().indexOf(keyword) !== -1
+			) !== -1;
+
+		return isCategoryMatched && isSearchMatched;
+	});
