0f4f65d8338b0ab808c8ef680eadb0ad2a4ed366
Eric <linmingy@usc.edu>
2020-05-22 17:33:52 -0700

    add helper function to validate date

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -14,3 +14,5 @@ export const filterTrips = (trips = [], searchKeyword = '', filterCategory = CAT
 
 		return isCategoryMatched && isSearchMatched;
 	});
+
+export const isValidDate = (date) => date instanceof Date && !isNaN(date);
