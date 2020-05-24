7e315842d94c46526a991fed03be6212ac4c8f34
Eric <linmingy@usc.edu>
2020-05-24 00:23:48 -0700

    update Todo and Trip model

--- src/common/Todo.js
+++ src/common/Todo.js
@@ -1,6 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class Todo {
 	constructor({ id, name, isComplete }) {
-		this.id = id || new Date().getTime();
+		this.id = id || uuidv4();
 		this.name = name || '';
 		this.isComplete = isComplete === true;
 	}
--- src/common/Trip.js
+++ src/common/Trip.js
@@ -18,10 +18,10 @@ export default class Trip {
 		this.category = category;
 		this.destination = destination;
 		this.description = description;
-		this.startDate = startDate ? new Date(startDate) : new Date();
-		this.endDate = endDate ? new Date(endDate) : new Date();
+		this.startDate = startDate ? new Date(startDate) : null;
+		this.endDate = endDate ? new Date(endDate) : null;
 		this.todos = Array.isArray(todos) ? todos.map((todo) => new Todo(todo)) : [];
-		this.reminder = reminder ? new Date(reminder) : '';
+		this.reminder = reminder ? new Date(reminder) : null;
 		this.status = status; // active,
 	}
 }
