0780d8f6e02cd496e985f116557729b4d95de092
Eric <linmingy@usc.edu>
2020-05-24 01:38:15 -0700

    wrap todo item and validate message in a component

---
+++ src/components/detailPanel/FormTodo.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { ListGroup, InputGroup, Form, Button } from 'react-bootstrap';
+
+const FormTodo = ({ id, isComplete, name, placeholder, onRemoveTodo, onChangeTodo }) => {
+	const [isValid, setIsValid] = useState(null);
+
+	const onBlur = ({ currentTarget }) => {
+		setIsValid(currentTarget.checkValidity());
+	};
+
+	const onChange = ({ target }) => {
+		const key = target.name;
+		const value = key === 'name' ? target.value : target.checked;
+
+		if (onChangeTodo) {
+			onChangeTodo(id, key, value);
+		}
+	};
+
+	const onClickTrashIcon = () => {
+		if (onRemoveTodo) {
+			onRemoveTodo(id);
+		}
+	};
+
+	return (
+		<ListGroup.Item key={id} className="detail-panel-trip-form-todoListItem">
+			<InputGroup>
+				<InputGroup.Prepend>
+					<InputGroup.Checkbox
+						aria-label="Checkbox for following text input"
+						checked={isComplete}
+						name="isComplete"
+						onChange={onChange}
+					/>
+				</InputGroup.Prepend>
+				<Form.Control
+					required
+					size="sm"
+					type="text"
+					placeholder={placeholder}
+					name="name"
+					value={name}
+					onBlur={onBlur}
+					className={isValid === false ? 'is-invalid' : ''}
+					onChange={onChange}
+				/>
+				<InputGroup.Append>
+					<Button size="sm" variant="danger" onClick={onClickTrashIcon}>
+						<i className="fa fa-trash-o" style={{ color: '#fff' }} />
+					</Button>
+				</InputGroup.Append>
+				<Form.Control.Feedback type="invalid">
+					Please type todo item names
+				</Form.Control.Feedback>
+			</InputGroup>
+		</ListGroup.Item>
+	);
+};
+
+export default FormTodo;
