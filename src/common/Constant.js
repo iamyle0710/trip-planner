8edd46f2200a86f222d338dc403eb3f3d73c0f52
Eric <linmingy@usc.edu>
2020-05-25 01:10:53 -0700

    add snooze options

--- src/common/Constant.js
+++ src/common/Constant.js
@@ -10,16 +10,26 @@ const TRIP_STATUS = {
 	READY: 'Ready',
 };
 
+const SNOOZE = {
+	TEN_SECS: { name: '10 seconds', value: 10 * 1000 },
+	FIVE_MINS: { name: '5 minutes', value: 60 * 5 * 1000 },
+	FIFTEEN_MINS: { name: '15 minutes', value: 60 * 15 * 1000 },
+	THIRTY_MINS: { name: '30 minutes', value: 60 * 30 * 1000 },
+	ONE_HOUR: { name: '1 hour', value: 60 * 60 * 1 * 1000 },
+	SIX_HOURS: { name: '6 hours', value: 60 * 60 * 6 * 1000 },
+	TWELEVE_HOURS: { name: '12 hours', value: 60 * 60 * 12 * 1000 },
+};
+
 const SAMPLE_DATA = [
 	{
 		id: '0',
 		title: 'go to lax',
 		destination: 'LAX',
 		description: 'here goes the description',
-		startDate: '2020-05-20',
-		endDate: '2020-05-25',
+		startDate: '2020/05/20',
+		endDate: '2020/05/25',
 		category: 'Vacation',
-		reminder: '2020-05-27T04:00:00.000Z',
+		reminder: '5/24/2020 2:39:30 PM',
 		status: 'Created',
 		todos: [
 			{
@@ -39,10 +49,10 @@ const SAMPLE_DATA = [
 		title: 'go to taiwan',
 		destination: 'Taiwan',
 		description: 'here goes the description',
-		startDate: '2020-05-20',
-		endDate: '2020-05-25',
+		startDate: '2020/05/20',
+		endDate: '2020/05/25',
 		category: 'Vacation',
-		reminder: '2020-05-28T04:00:00.000Z',
+		reminder: '5/24/2020 2:57:50 PM',
 		status: 'Created',
 		todos: [
 			{
@@ -65,7 +75,7 @@ const SAMPLE_DATA = [
 		startDate: '2020-05-20',
 		endDate: '2020-05-25',
 		category: 'Business',
-		reminder: '2020-05-30T04:00:00.000Z',
+		reminder: '5/25/2020 2:11:36 PM',
 		status: 'Created',
 		todos: [
 			{
@@ -85,5 +95,6 @@ const SAMPLE_DATA = [
 export default {
 	CATEGORY,
 	SAMPLE_DATA,
+	SNOOZE,
 	TRIP_STATUS,
 };
